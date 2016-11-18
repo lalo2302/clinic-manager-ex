1b7a7a121839caca7f9dccd710aff8be2b52f726
Eduardo Aguilera <laloao.2302@gmail.com>
2016-11-18 00:54:53 -0600

    Add factory of patient, user, clinic and employee

--- mix.exs
+++ mix.exs
@@ -18,20 +18,19 @@ defmodule ClinicApp.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {ClinicApp, []},
-     applications: [:faker, :ex_machina, :phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+     applications: [:faker, :ex_machina, :phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext, :phoenix_ecto, :postgrex]]
   end
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
-  defp elixirc_paths(_),     do: ["lib", "web"]
+  defp elixirc_paths(_),     do: ["lib", "web", "test/support"]
 
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:faker, "~> 0.7", only: :test},
-     {:ex_machina, "~> 1.0", only: :test},
+    [{:faker, "~> 0.7"},
+     {:ex_machina, "~> 1.0"},
      {:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,9 +2,59 @@ defmodule ClinicApp.Factory do
   use ExMachina.Ecto #, repo: ClinicApp.Repo
 
   def patient_factory do
+    %ClinicApp.Patient{
+      name: Faker.Name.first_name,
+      last_name: Faker.Name.last_name,
+      date_of_birth: DateTime.utc_now,
+      gender: "M",
+      curp: "AUOE960223HDFGLD00",
+      rfc: "QCS931209 G49",
+      address: Faker.Address.street_address,
+      email: Faker.Internet.free_email,
+      phone: Faker.Phone.EnUs.phone 
+
+      # Relation with ClinicalHistory
+      # Relation with User
+    }
+  end
+
+  def user_factory do
     %ClinicApp.User{
-      username: Faker.Internet.user_name
+      username: Faker.Internet.user_name,
       password: Faker.Lorem.characters(9)
     }
   end 
+
+  def clinic_factory do
+    %ClinicApp.Clinic{
+      name: Faker.Company.name,
+      address: Faker.Address.street_address,
+      phone: Faker.Phone.EnUs.phone,
+      manager: Faker.Name.name,
+      email: Faker.Internet.free_email,
+      open_time: Time.new(8,0,0),
+      closing_time: Time.new(17,0,0),
+      open_days: "Lun-Vie"
+    }
+  end
+
+  def employee_factory do
+    %ClinicApp.Employee{
+      name: Faker.Name.name,
+      last_name: Faker.Name.last_name,
+      date_of_birth: Date.new(1996, 2, 23),
+      gender: "M",
+      curp: "AUOE960223HDFGLD00",
+      rfc: "QCS931209 G49",
+      address: Faker.Address.street_address,
+      email: Faker.Internet.free_email,
+      phone: Faker.Phone.EnUs.phone,
+      profession: Faker.Company.bullshit,
+      job: Faker.Name.title_job
+
+      # Specialty 1
+      # User 1
+    }
+  end
+  # TODO: factories
 end
