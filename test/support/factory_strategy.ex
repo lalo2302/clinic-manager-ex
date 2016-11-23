64fcb905e97f903e03ef1aa3b0fcf9b921f797e2
Eduardo Aguilera <laloao.2302@gmail.com>
2016-11-22 23:20:31 -0600

    Change exMachina strategy to json encode

---
+++ test/support/factory_strategy.ex
@@ -0,0 +1,8 @@
+defmodule ClinicApp.JsonEncodeStrategy do
+
+  use ExMachina.Strategy, function_name: :json_encode
+
+  def handle_json_encode(record, _opts) do
+    Poison.encode!(record)
+  end
+end
